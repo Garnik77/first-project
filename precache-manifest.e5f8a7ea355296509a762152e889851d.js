cba07b3f520534fa3f894f02f393f3a06f58ae1f
Garnik77 <garnop@mail.ru>
2020-02-26 22:42:24 +0400

    Updates

---
+++ precache-manifest.e5f8a7ea355296509a762152e889851d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91971b7243a957cbf51e316f747b54e2",
+    "url": "/first-project/index.html"
+  },
+  {
+    "revision": "fde8477070559faa7587",
+    "url": "/first-project/static/css/3.371db902.chunk.css"
+  },
+  {
+    "revision": "b0f9e8e3de021a2d926f",
+    "url": "/first-project/static/css/4.9a79a92b.chunk.css"
+  },
+  {
+    "revision": "a51f340f29f0fc55f393",
+    "url": "/first-project/static/css/main.033c929a.chunk.css"
+  },
+  {
+    "revision": "29172198748692a753e2",
+    "url": "/first-project/static/js/2.e03430e5.chunk.js"
+  },
+  {
+    "revision": "fde8477070559faa7587",
+    "url": "/first-project/static/js/3.8927395c.chunk.js"
+  },
+  {
+    "revision": "b0f9e8e3de021a2d926f",
+    "url": "/first-project/static/js/4.2da654e9.chunk.js"
+  },
+  {
+    "revision": "a51f340f29f0fc55f393",
+    "url": "/first-project/static/js/main.aaf62071.chunk.js"
+  },
+  {
+    "revision": "0bf64b1d7701424f0312",
+    "url": "/first-project/static/js/runtime-main.4673aabc.js"
+  },
+  {
+    "revision": "3e17cba4d04d694b4157ce3861331619",
+    "url": "/first-project/static/media/6.3e17cba4.gif"
+  }
+]);
